10a8e4ef26d9819a17966bd1e254f1626220c14e
koushik ghosh <kg3540213@gmail.com>
2025-07-05 08:22:02 +0530

    new

--- projects/addtaslinlist/task.js
+++
@@ -1,9 +0,0 @@
-const inp = document.getElementById('value');
-const sub = document.getElementById('submit');
-const ul = document.querySelector('ul');
-
-sub.addEventListener('click', () => {
-    const li = document.createElement('li');
-    li.innerHTML = inp.value;
-    ul.appendChild(li);
-});
